3718db5d53ad3a03098a8d3c715fd8a3a7ed32b8
Ratchanon Ruensuk <95856390+63160058@users.noreply.github.com>
2024-05-27 12:56:10 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+    <title>PHP Starter</title>
+</head>
+
+<body>
+    <?php 
+    function number() {
+        $Level=""; //ตัวแปรที่ใช้เก็บผลลัพธ์ที่จะนำมา output
+        $number = array("2","6","88","99","22","1","76","3"); //ตัวแปรเก็บค่า array ที่ใช้ input เข้าไปในเเต่ละ Level
+        $levels = array("1","2","4","8"); //ตัวแปรเก็บค่า array ที่ใช่ในการกำหนดขนาดในเเต่ละ Level
+        $num = 0; //ตัวแปรเพื่อเก็บตำแหน่งปัจจุบันในอาร์เรย์ $numberูณทั้งหมดำแหน่งปัจจุบันในอาร์เรย์ $number< count($levels) ; $i++){ 
+        $Level .= //ตัวแปรเก็บค่าผลรวมจากการคูณทั้งหมดvel_values = array(); 
+        $sumLevel=1; 
+       
+        for($e=0 ## สร้างลูปตามจำนวนสร้างข้อ## //ลูปตามจำนวนข้อมูลใน array ของตัวแปร $levels ปร $levels ; $e< $levels[$i] ; $e++){ 
+            if($num < count($number)){ 
+       //ตัวแปรที่ใช้เก็บผลลัพธ์ท//ตัวแปรเก็บค่า array ในเเต่ละ Levelี่จะนำมา outputก็บค่า array ในเเต่ละ Levelคูณในเเตละ Level                     sort($level_values); ##เรียงข้อมูลในเเต่ละ Level จากน้อยไปมาก
+        $Levee .= im //ตัวแปรเก็บผลคูณในเเต่ละ Levelplode(","ลคูณ: " .$sumLevel;
+        $Level .= "<br>";
+        $sum += $sumLel;
+
+       
+    
+    //ลูปตามจำนวนข้อมูลเ//ตั้งเงื่อ//ต///ัวแปรเก็บค่าผลคูณในเเต่ละ Levelัวแปรเก็บค่า array ในเเต่ละ Levelนไขเพื่อเก็บค่า array ตามขนาดในเเต่ละ Level เเละจำนวนข้อมูลของ $numberเต่ละตำเเหน่งใน array ของตัวแปร $levels ls }
+    if($sue%2 == 0){
+       $a=" is even"; ##เขคู่
+    }else{
+        $a=" is o //เรียงข้อมูลในเเต่ละ Level จากน้อยไปมากdd"; ##เลขคี่
+    }
+    esum  .e "$a";
+    echo $Leeel;
+    echo $sum;
+    
+}
+
+number();
+?>
+</body>
+
+</html>
